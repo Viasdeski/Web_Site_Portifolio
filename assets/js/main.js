96ba739202c656b7361b5ea66043a2d768340357
Viasdeski <vitor.viasdeski@hotmail.com>
2024-04-22 23:05:07 -0300

    Add Services Section e Services Modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,3 +63,27 @@ function PortfolioItemDetails(portfolioItem){
     document.querySelector('.portfolio__popup-body span').innerHTML = portfolioItem.querySelector('.portfolio__item-details').innerHTML;
 
 }
+
+
+/*---SERVICES MODAL---*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
