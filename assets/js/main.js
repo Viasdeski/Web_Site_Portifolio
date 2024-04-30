e91a72f30704d88bc7f9bf8d36fba8a1dab8fa39
Viasdeski <vitor.viasdeski@hotmail.com>
2024-04-29 21:58:51 -0300

    Add Breakpoints and Navbar Toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
+/*--SHOW SIDEBAR--*/
+const navMenu = document.getElementById('sidebar'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-sidebar')
+    })
+}
+
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-sidebar')
+    })
+}
+
+
+
 /*---SKILLS TABS---*/
 const tabs = document.querySelectorAll('[data-target]'),
       tabContent = document.querySelectorAll('[data-content]');
